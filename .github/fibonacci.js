d2e4a54fc831c310f15c57c030c40fe5b3a9f17e
MatiIL <matibres@gmail.com>
2022-06-21 12:45:13 +0300

    Milestone2

--- .github/fibonacci.js
+++ .github/fibonacci.js
@@ -1,4 +1,28 @@
-let x = document.querySelector("span");
-let y = document.querySelector("p");
-x.textContent += 5;
-y.textContent += 3;
+let fibNumIndex = document.querySelector("span");
+let fibNum = document.querySelector("p");
+
+function fibNumCalc(x) {
+    let yAntecedent = 0;
+    let y = 1;
+    if (x <= 0)
+    return null;
+    else if (x === 1)
+    return yAntecedent;
+         else for(let i = 2; i < x; i++) {
+            let preCedingNums = y + yAntecedent;
+            yAntecedent = y;
+            y = preCedingNums;
+    }
+    return y;   
+}
+
+let x = 8;
+fibNumIndex.innerText += x;
+fibNum.innerText += fibNumCalc(x);
+
+
+
+
+
+
+
