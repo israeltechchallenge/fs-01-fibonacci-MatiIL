9ed221d7507d4a5e89b2d2587cc58a031235e04b
MatiIL <matibres@gmail.com>
2022-06-23 21:49:10 +0300

    JS Project Milestone5 MB

--- .github/fibonacci.js
+++ .github/fibonacci.js
@@ -1,21 +1,57 @@
 const calcBtn = document.getElementById('isbutton');
 const outputNum = document.getElementById('user-output');
 const userInput = document.getElementById('user-input');
+const spinnerBtn = document.getElementById('spinner');
+const alert = document.getElementById('alert');
 
 calcBtn.addEventListener('click', function(e) {
     e.preventDefault();
     let fibNumIndex = userInput.value;
-    return callFibServer(fibNumIndex);
+    if (fibNumIndex > 50) {
+        alert.classList.remove('visually-hidden');
+        return 0;
+    } else {
+        spinnerBtn.classList.remove('visually-hidden');
+        return callFibServer(fibNumIndex);
+    }
 })
 
 function callFibServer(fibNumIndex) {
 const SERVER_URL = `http://localhost:5050/fibonacci/${fibNumIndex}`;
 fetch(SERVER_URL)
-.then(function(response) {
-    console.log(response);
-    return response.json(); 
+.then((response) => {
+    if (!response.ok) {
+        response.text().then((errorText) => {
+            outputNum.innerText = "Server Error: " + errorText;
+            outputNum.classList.add('text-danger');
+            outputNum.classList.add('fs-6');
+        });
+    } else {
+
+        return response.json();
+    }
+    
   })
 .then(function(data) {
+    spinnerBtn.classList.add('visually-hidden');
     outputNum.innerHTML = data.result;
   });
 }
+
+// spinnerBtn.classList.add('d-none');
+// spinnerBtn.classList.remove('.d-none');
+// function displaySpinner() {
+//     spinnerBtn.classList.remove('visually-hidden');
+// }
+
+// function loadFibNum() {
+//     displaySpinner();
+//     const SERVER_URL = `http://localhost:5050/fibonacci/${fibNumIndex}`;
+// fetch(SERVER_URL)
+// .then(function(response) {
+//     return response.json(); 
+//   })
+// .then(function(data) {
+//     outputNum.innerHTML = data.result;
+//   });
+
